90017cd9131b3fa58e6eff58d10a25a1b56f232e
Mirage <mirage.abeysekara@gmail.com>
2015-03-21 11:38:27 +0530

    Added pages for register doctor and patient

--- eChannel/App_Start/RouteConfig.cs
+++ eChannel/App_Start/RouteConfig.cs
@@ -13,10 +13,28 @@ namespace eChannel
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "Home",
+                url: "",
+                defaults: new { controller = "Home", action = "Index"}
+            );
+
+            routes.MapRoute(
+                name: "RegisterAsDoctor",
+                url: "register-doctor",
+                defaults: new { controller = "User", action = "RegisterDoctor" }
+            );
+
+            routes.MapRoute(
+                name: "RegisterAsPatient",
+                url: "register-patient",
+                defaults: new { controller = "User", action = "RegisterPatient" }
             );
         }
     }
---
+++ eChannel/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eChannel.Controllers
+{
+    public class UserController : Controller
+    {
+      
+        public ActionResult RegisterDoctor()
+        {
+            if (Request.HttpMethod.Equals("POST"))
+            {
+                //ViewData["username"] = Request.Form["username"];
+            }
+
+            return View();
+        }
+
+        public ActionResult RegisterPatient()
+        {
+            return View();
+        }
+	}
+}
